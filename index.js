60b5de0d40f89a414fbd7879483e81e4a2a3d663
Allison Howard <alliemhow@gmail.com>
2019-08-27 09:00:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ class Route {
 
   estimatedTime(peak) {
     if (peak) {
-      this.blocksTravelled()/3
+      return this.blocksTravelled()/3
     } else {
       this.blocksTravelled()/2
     }
