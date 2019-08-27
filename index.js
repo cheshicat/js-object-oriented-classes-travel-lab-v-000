49b933de1e1b91e25ad441363108fe5883b27883
Allison Howard <alliemhow@gmail.com>
2019-08-27 08:07:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ class Driver {
     this.startDate = new Date()
   }
   yearsExperienceFromBeginningOf(endDate) {
-    this.endDate - this.startDate.getFullYear()
+    return (this.endDate - this.startDate.getFullYear())
   }
 }
