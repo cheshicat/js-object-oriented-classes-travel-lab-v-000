cee0240e7a40a8e4a8f9acf2797867ca44ef12a7
Allison Howard <alliemhow@gmail.com>
2019-08-27 08:37:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,8 @@ class Driver {
 }
 
 class Route {
-
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
 }
