cc0029d5caf6621cf45b965d032123760a234615
Allison Howard <alliemhow@gmail.com>
2019-08-27 08:54:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ class Route {
   }
 
   blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
     let vertical = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
     let horizontal = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
 
